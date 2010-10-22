441bc7d0e214db3908184f6724933d4f62b19cdd
Alex Kovar <ajkovar@gmail.com>
2010-10-22 17:09:03 -0500

    formatObject

--- spec/javascripts/HtmlFormatterSpec.js
+++ spec/javascripts/HtmlFormatterSpec.js
@@ -1,12 +1,22 @@
 describe("HtmlFormatter", function() {
-  var array;
+  var jsonArray, jsonObject;
 
   beforeEach(function() {
-    array = [{a:"a", b: "b"}]
+    jsonObject = {a:"a", b: "b"}
+    jsonArray = [jsonObject]
   });
 
-  it("should format an array of elements", function() {
-    expect(HtmlFormatter.formatArray(array)).toEqual("<table><thead><tr><th>a</th><th>b</th></tr></thead><tbody><tr><td>a</td><td>b</td></tr></tbody></table>")
+  it("should format an jsonArray of elements", function() {
+    expect(HtmlFormatter.formatArray(jsonArray)).toEqual("<table><thead><tr><th>a</th><th>b</th></tr></thead><tbody><tr><td>a</td><td>b</td></tr></tbody></table>")
   })
 
+  it("should ignore array properties", function() {
+    jsonObject.c=[1,2,3]
+    expect(HtmlFormatter.formatObject(jsonObject)).toEqual("<table><thead><tr><th>a</th><th>b</th></tr></thead><tbody><tr><td>a</td><td>b</td></tr></tbody></table>")
+  })
+
+  it("should ignore object properties", function() {
+    jsonArray[0].c={}
+    expect(HtmlFormatter.formatObject(jsonObject)).toEqual("<table><thead><tr><th>a</th><th>b</th></tr></thead><tbody><tr><td>a</td><td>b</td></tr></tbody></table>")
+  })
 })
